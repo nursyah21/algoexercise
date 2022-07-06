0ad76034eeb91c9cb6f955567be65b2ace1073eb
iniryan <riyanadi552@gmail.com>
2022-07-06 15:20:47 +0700

    Create main.cpp

---
+++ 1201210006 Ryan Adi Saputra/Basic Data Types/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+  
+    scanf("%d %lld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n%lld\n%c\n%f\n%lf", a, b, c, d, e);
+    return 0;
+}
