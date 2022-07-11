fe67bd329dcfe4015f003793d9781028380751b1
nursyahjaya <nursyahjaya@gmail.com>
2022-07-11 09:54:04 +0700

    update generate.sh

--- generate.sh
+++ generate.sh
@@ -6,7 +6,7 @@ header(){
 
   echo "![](https://github.com/nursyah21/algoexercise/workflows/CodeQL/badge.svg)"
 
-  printf "good resource to exercise :\n"
+  printf "\ngood resource to exercise :\n"
   echo "- [basic cpp hackerrank](https://www.hackerrank.com/domains/cpp?filters%5Bdifficulty%5D%5B%5D=easy)  (min: solve 14 question)"
   echo "- [algorithm projecteuler](https://projecteuler.net/archives) (min: solve 6 question)"
   echo "- [top easy interview leetcode](https://leetcode.com/explore/interview/card/top-interview-questions-easy/)"
