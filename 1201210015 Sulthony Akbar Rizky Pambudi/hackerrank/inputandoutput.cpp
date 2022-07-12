880a0622d0cd2c6889c3483324fe323ad4dd1fa5
sulthonyakbar <77472017+sulthonyakbar@users.noreply.github.com>
2022-07-12 19:16:36 +0700

    Update inputandoutput.cpp

--- 1201210015 Sulthony Akbar Rizky Pambudi/hackerrank/inputandoutput.cpp
+++ 1201210015 Sulthony Akbar Rizky Pambudi/hackerrank/inputandoutput.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-#include <cstdio>
 using namespace std;
 
 int main() 
 {
     int a;
-    long int b;
-    char c;
-    float d;
-    double e;
-
-    scanf("%d %lld %c %f %lf",&a,&b,&c,&d,&e);
-
-    printf("%d\n%lld\n%c\n%f\n%lf",a,b,c,d,e);
-
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    
+    cout << a + b + c << endl;
+    
     return 0;
 }
