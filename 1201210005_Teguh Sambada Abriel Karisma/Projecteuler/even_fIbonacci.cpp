9dcbf518cb78e2fcee987de1e833866b1f441eb8
abrielkarisma <abrielcha.ac@gmail.com>
2022-07-14 21:56:41 +0700

    finish

--- 1201210005_Teguh Sambada Abriel Karisma/Projecteuler/even_fIbonacci.cpp
+++ 1201210005_Teguh Sambada Abriel Karisma/Projecteuler/even_fIbonacci.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
   unsigned int tests;
-  std::cin >> tests;
+  cin >> tests;
   while (tests--)
   {
     unsigned long long last;
     std::cin >> last;
 
     unsigned long long sum = 0;
-    // first Fibonacci numbers
     unsigned long long a = 1;
     unsigned long long b = 2;
-
-    // until we reach the limit
     while (b <= last)
     {
-      // even ?
       if (b % 2 == 0)
         sum += b;
-
-      // next Fibonacci number
       auto next = a + b;
       a = b;
       b = next;
     }
 
-    std::cout << sum << std::endl;
+    cout << sum << std::endl;
   }
   return 0;
 }
--- 1201210005_Teguh Sambada Abriel Karisma/Projecteuler/summations_of_prime.cpp
+++ 1201210005_Teguh Sambada Abriel Karisma/Projecteuler/summations_of_prime.cpp
@@ -3,25 +3,19 @@
 using namespace std;
 
 int main(int argc, char const *argv[])
-{
-    // upper limit & sum
+{\
     int sum = 0;
     int upLimit = 2000000;
-
-    // array of isPrime check for integers
     bool isPrime[upLimit+1];
 
-    // initialize
     for (int i=2; i <= upLimit; i++) {
         isPrime[i] = true;
     }
 
     for (int i=2; i<=upLimit; i++) {
-        if (isPrime[i]) {
-            // add to sum
+        if (isPrime[i])
             sum += i;
 
-            // mark all multiples
             int j = i;
             while (j <= upLimit) {
                 isPrime[j] = false;
