0c210b2bd2cd15b0fb65236964b9f3831b36ed92
fidiramadhina <9hwahyufidi23@gmail.com>
2022-07-14 11:50:37 +0700

    fix soal 4

--- 1201210013_WahyuFidiRamadhina/hackerrank/soal4.cpp
+++ 1201210013_WahyuFidiRamadhina/hackerrank/soal4.cpp
@@ -38,3 +38,25 @@ int main()
 
     return 0;
 }
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
