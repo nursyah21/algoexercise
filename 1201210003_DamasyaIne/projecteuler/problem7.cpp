f43cd792ef03e05b09593e1a102f8e8e35f59516
Damasya <93261918+damasyaine07@users.noreply.github.com>
2022-07-15 06:18:08 +0700

    Update problem7.cpp

--- 1201210003_DamasyaIne/projecteuler/problem7.cpp
+++ 1201210003_DamasyaIne/projecteuler/problem7.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
-using namspace std;
-
-int main()
-{
-  int flag=(),count=1,n=2,n;
-  
-  while(count<=10001)
-  {
-    n=n/2;
-    flag=0;
-    for(int i=2;i<=n;i++)
-    {
-      if(n%i==0)
-      {
-        flag=1;
-      }
+using namespace std;
+int main() {
+    int flag=0,count=1,n=2,m;
+    
+    
+    while(count<=10001){
+        m=n/2;
+        flag=0;
+        for(int i=2;i<=m;i++){
+            if(n%i==0){
+                flag=1;
+            }
+        }
+        if(flag==0){
+            if(count==10001){
+                count<<n;
+            }
+            count++;
+        }
+        n++;
     }
-    if(flag==0)
-    {
-      if(count==1000)
-      {
-        cout << n;
-      }
-      cout++;
-    }
-    n++;
-  }
-  return 0'
-  }
+    return 0;
+}
