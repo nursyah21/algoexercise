62fb60a1357ab1b4be4c1df03ca8d6ea80bd51b1
fidiramadhina <9hwahyufidi23@gmail.com>
2022-07-14 11:34:42 +0700

    reupload untuk fix problem

---
+++ 1201210013 Wahyu Fidi Ramadhina/projectuler/Sum_square_difference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum_square = 0, square_sum = 0, total;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        sum_square += i;
+        square_sum += (i * i);
+    }
+
+    sum_square = sum_square * sum_square;
+
+    total = sum_square - square_sum;
+    cout << total;
+}
