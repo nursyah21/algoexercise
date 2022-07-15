ea917cca1feec4301085e71f6019b7bbce1f240b
akbarrizkip <akbar.rizki.21@student.se.ittelkom-sby.ac.id>
2022-07-15 16:20:37 +0700

    new repo

---
+++ 1201212021_Kresna_Agung_ws/hackerrank/Basic_Data_Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long w;
+    char s;
+    float d;
+    double r;
+    
+scanf("%d %ld %c %f %lf", &a, &w, &s, &d, &r);
+printf("%d\n%ld\n%c\n%f\n%lf\n", a , w, s, d, r);
+
+    return 0;
+}
---
+++ 1201212021_Kresna_Agung_ws/projecteuler/question 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+
+    int previousFibNum1 = 0;
+    int currentFibNum = 1;
+
+    int fibNumSum = 0;
+
+    while(currentFibNum <= 4000000)
+    {
+
+        fibNumSum += (currentFibNum % 2 == 0) ?
+            currentFibNum : 0;
+
+
+        int previousFibNum2 = previousFibNum1;
+        previousFibNum1 = currentFibNum;
+        currentFibNum = previousFibNum2 + previousFibNum1;
+    }
+
+    std::cout << fibNumSum << "\n";
+}
