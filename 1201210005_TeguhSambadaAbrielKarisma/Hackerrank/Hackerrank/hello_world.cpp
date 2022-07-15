707076b8fcba6aa92d407579d34cd6440c2ccfce
abrielkarisma <abrielcha.ac@gmail.com>
2022-07-15 18:44:21 +0700

    fix summation

--- 1201210005_TeguhSambadaAbrielKarisma/Hackerrank/Hackerrank/hello_world.cpp
+++ 1201210005_TeguhSambadaAbrielKarisma/Hackerrank/Hackerrank/hello_world.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World";
+    cout << "hello world";
     return 0;
 }
--- 1201210005_TeguhSambadaAbrielKarisma/Projecteuler/summations_of_prime.cpp
+++ 1201210005_TeguhSambadaAbrielKarisma/Projecteuler/summations_of_prime.cpp
@@ -2,26 +2,26 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    int sum = 0;
-    int upLimit = 2000000;
-    bool isPrime[upLimit+1];
+int main(int argc, char const *argv[]) 
+    {
+        int sum = 0;
+        int upLimit = 2000000;
+        bool isPrime[upLimit+1];
 
-    for (int i=2; i <= upLimit; i++) {
-        isPrime[i] = true;
-    }
-
-    for (int i=2; i<=upLimit; i++) {
-        if (isPrime[i])
-            sum += i;
-
-            int j = i;
-            while (j <= upLimit) {
-                isPrime[j] = false;
-                j += i;
-            }
+        for (int i=2; i <= upLimit; i++) {
+            isPrime[i] = true;
         }
-            cout << sum << endl;
+
+        for (int i=2; i<=upLimit; i++) {
+            if (isPrime[i])
+                sum += i;
+
+                int j = i;
+                while (j <= upLimit) {
+                    isPrime[j] = false;
+                    j += i;
+                }
+            }
+                cout << sum << endl;
     }
 
